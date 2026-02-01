9e2b2f0b91f83a962239e128bf7fa5a0646f8d1f
yuvashree112112 <24z379@psgtech.ac.in>
2026-02-01 14:05:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function append(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    let display = document.getElementById("display");
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
 
